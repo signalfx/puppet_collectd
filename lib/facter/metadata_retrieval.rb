d60ac3a28bcaca229f05dbd29d2f8f09dbb7cd5e
Alexander Ospina <alexanderospina@Alexanders-MacBook-Air.local>
2016-12-14 17:21:47 -0500

    creating a new fact to pull data from

---
+++ lib/facter/metadata_retrieval.rb
@@ -0,0 +1,18 @@
+require 'net/http'
+uri = URI.parse('http://169.254.169.254/2014-11-05/dynamic/instance-identity/document')
+http = Net::HTTP.new(uri.host, uri.port)
+http.open_timeout = 4
+http.read_timeout = 4
+begin
+  http.start
+  response = http.request(Net::HTTP::Get.new(uri.request_uri))
+rescue StandardError
+  raise 'Unable to reach AWS metadata'
+end
+unless response.nil? || response.zero?
+  Facter.add('ec2_instance_identity_document') do
+    setcode do
+      JSON.parse(response.body).to_json
+    end
+  end
+end
